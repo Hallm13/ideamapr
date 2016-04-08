9d2e815cd86dfed64dac31c1913570d834ca0b6e
Sameer Siruguri <siruguri@gmail.com>
2016-04-08 08:57:40 -0700

    Corrected raw SQL to work for Postgres

--- lib/sql_optimizers.rb
+++ lib/sql_optimizers.rb
@@ -5,7 +5,7 @@ module SqlOptimizers
       groupable = args[0]
       groupable_type = args[0].class.to_s
 
-      sql = "insert into idea_assignments  ('groupable_type', 'groupable_id', 'idea_id') values "
+      sql = "insert into idea_assignments  (groupable_type, groupable_id, idea_id) values "
       sql += args[1].map do |idea_id|
         "('#{groupable_type}', #{args[0].id}, #{idea_id})"
       end.join(', ')
