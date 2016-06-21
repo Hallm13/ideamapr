b71edd7d7b0313d4007e0537908bbc5660262f50
Sameer Siruguri <siruguri@gmail.com>
2016-06-21 11:29:51 -0700

    One more test passes

--- test/features/public_survey_test.rb
+++ test/features/public_survey_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 class PublicSurveyTest < Capybara::Rails::TestCase
   before do
-    Capybara.default_driver = :webkit
+    Capybara.default_driver = :selenium
   end
   
   describe 'pro/con' do
     before do
       @s = surveys(:published_survey)
-      @base_url = '/survey/public_show/' + @s.public_link
+      @base_url = '/surveys/public_show/' + @s.public_link
       visit @base_url
     end
 
     it 'cycles through questions' do
       page.find('#go-right').click
       assert page.has_content? 'pre5 '
-      assert page.has_css?('.idea-title', visible: true, count: 1)
+      assert page.has_css?('.idea-box', visible: true, count: 1)
     end
 
     it 'gets to thank you screen' do
--- test/services/ajax_controller_test.rb
+++ test/services/ajax_controller_test.rb
@@ -13,7 +13,7 @@ module Ajax
 
     test 'getting help text' do
       data = Ajax::Library.route_action("cms/get/help_text")
-      assert_equal 2, data[:data].count
+      assert_equal CmsContent.count, data[:data].count
     end
 
     test 'getting cms content' do
