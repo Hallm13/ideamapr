04d6badd0e21571c7f22a8721f6e593cf820bdc1
Sameer Siruguri <siruguri@gmail.com>
2016-08-11 15:51:34 -0700

    Fix up partic navbar mobile a bit
    * Fixture error

--- test/controllers/public_info_controller_test.rb
+++ test/controllers/public_info_controller_test.rb
@@ -5,10 +5,12 @@ class PublicInfoControllerTest < ActionController::TestCase
     get :double_bundle, for_survey: surveys(:published_survey).public_link
     b = JSON.parse response.body
 
+    # order = procon, radio, new idea, budget, ranking
     # The radio choice question is the 2nd one assigned to published_survey
     assert_equal 'detail', b['list_of_lists'][1]['type']
 
     # 1st idea in 3rd question has a budget amount in the fixtures
+
     assert_equal 500.0, b['list_of_lists'][3]['data'][0]['cart_amount']
   end
 end
